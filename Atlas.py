2a04abae2856a5a23308fcb44361314bda971329
ClosePrize <circimustafaa@gmail.com>
2022-10-28 18:09:46 +0300

    123

--- Atlas.py
+++ Atlas.py
@@ -1,6 +1,8 @@
-from pymavlink import mavutil 
+from pymavlink import mavutil, mavwp
 
-address = 'udpin:localhost:14551' #simulasyon
+wp= mavwp.MAVWPLoader()
+
+address = 'udpin:0.0.0.0:9000' #simulasyon
 # address= '/dev/ttyACM0' #pixhawk usb
 # address= '/dev/ttyTHS1' #pixhawk telem2 baudrate= 115200
 vehicle= mavutil.mavlink_connection(address,baudrate=57600,autoreconnect= True)
@@ -11,4 +13,29 @@ def get_alt():
     message = vehicle.recv_match(type='GLOBAL_POSITION_INT', blocking= True)
     alt=message.relative_alt
     alt = alt/1000
-    return al
+    return alt
+
+def takeoff(alt):
+    vehicle.mav.command_long_send(vehicle.target_system, vehicle.target_component,mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, alt)
+    while True: 
+        current_alt= get_alt()
+        if current_alt< alt:
+            print(f"Anlik irtifa {current_alt}")
+        elif current_alt >=  alt:
+            print("Istenilen irtifaya ulasildi ")
+            break
+
+def add_mission(seq,lat,lon,alt):
+    frame= mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
+    wp.add(mavutil.mavlink.MAVLink_mission_item_message(vehicle.target_system, vehicle.target_component,
+    seq,
+    frame,
+    mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,0,0,0,0,0,0,lat,lon,alt))
+
+    vehicle.waypoint_clear_all_send()
+    vehicle.waypoint_count_send(wp.count())
+    for i in range (wp.count()):
+        msg= vehicle.recv_match(type=["MISSION_REQUEST"], blocking= True)
+        vehicle.mav.send(wp.wp(msg.seq))
+        print("Sending waypoints {0}".format(msg.seq))
+
